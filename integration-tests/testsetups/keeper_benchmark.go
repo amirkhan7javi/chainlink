b9383008bb15b4249403dd4bde58fd462a26a96b
anirudhwarrier <anirudhwarrier@users.noreply.github.com>
2022-09-30 13:07:44 +0530

    try upkeep reset during run

--- integration-tests/testsetups/keeper_benchmark.go
+++ integration-tests/testsetups/keeper_benchmark.go
@@ -152,12 +152,26 @@ func (k *KeeperBenchmarkTest) Run() {
 		// Send keeper jobs to registry and chainlink nodes
 		actions.CreateKeeperJobsWithKeyIndex(k.chainlinkNodes, k.keeperRegistries[rIndex], rIndex)
 		for index, keeperConsumer := range k.keeperConsumerContracts[rIndex] {
+			err := keeperConsumer.SetFirstEligibleBuffer(context.Background(), big.NewInt(1))
+			if err != nil {
+				log.Error().Err(err).Msg("Error setting first eligible buffer of consumer contract")
+				return
+			}
+			err = k.chainClient.WaitForEvents()
+			Expect(err).ShouldNot(HaveOccurred(), "Failed waiting for setting first eligible buffer of consumer contract")
+			err = keeperConsumer.Reset(context.Background())
+			if err != nil {
+				log.Error().Err(err).Msg("Error resetting consumer contract")
+				return
+			}
+			err = k.chainClient.WaitForEvents()
+			Expect(err).ShouldNot(HaveOccurred(), "Failed waiting for resetting consumer contract")
 			k.chainClient.AddHeaderEventSubscription(fmt.Sprintf("Keeper Tracker %d %d", rIndex, index),
 				contracts.NewKeeperConsumerBenchmarkRoundConfirmer(
 					keeperConsumer,
 					k.keeperRegistries[rIndex],
 					k.upkeepIDs[rIndex][index],
-					k.Inputs.BlockRange+k.Inputs.FirstEligibleBuffer+k.Inputs.BlockInterval,
+					k.Inputs.BlockRange+k.Inputs.BlockInterval,
 					k.Inputs.UpkeepSLA,
 					&k.TestReporter,
 				),
