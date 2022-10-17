51a4da3c35c93895c924dde2e6a66451fd00b98b
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2022-10-17 12:05:42 -0700

    Fix lint errors introduced from merge

--- core/services/ocr2/plugins/dkg/config/config_test.go
+++ core/services/ocr2/plugins/dkg/config/config_test.go
@@ -6,8 +6,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/smartcontractkit/chainlink/core/internal/cltest"
 	configtest "github.com/smartcontractkit/chainlink/core/internal/testutils/configtest/v2"
 	"github.com/smartcontractkit/chainlink/core/internal/testutils/pgtest"
